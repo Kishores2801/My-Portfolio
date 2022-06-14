60294a77385bac87ec259e0b500778c91f01c073
Kishore <kishoreshan2801@gmail.com>
2022-06-14 11:47:41 +0530

    updated setting to include local host

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@54yf7g2b8jn)+-pfs2ge9h^(hts6th$a*7rmp68vg1fegf)sp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = FALSE
 
-ALLOWED_HOSTS = ["ks-ds-portfolio.herokuapp.com"]
+ALLOWED_HOSTS = ["ks-ds-portfolio.herokuapp.com",'127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
